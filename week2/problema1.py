190d79171c004113a46886767139760495ca4660
Gilnior <gilnior123@gmail.com>
2021-10-25 11:53:36 -0300

    problema 1 da week2 resolvido

--- week2/problema1.py
+++ week2/problema1.py
@@ -5,25 +5,13 @@ Faça um programa que leia cinco números inteiros e identifique:
 • Quantos números são divisíveis por 3
 """
 
-# n1 = int(input("Digite o primeiro inteiro: "))
-# n2 = int(input("Digite o segundo inteiro: "))
-# n3 = int(input("Digite o terceiro inteiro: "))
-# n4 = int(input("Digite o quarto inteiro: "))
-# n5 = int(input("Digite o quinto inteiro: "))
-
 val = ["primeiro", "segundo", "terceiro", "quarto", "quinto"]
 inteiros = sorted([int(input(f"Digite o {i} inteiro: ")) for i in val])
 
-RESPOSTA = (
-    "Maior: {maior}\nMenor: {menor}\nQuantidade de divisíveis por 3: {divisiveis_3}"
-)
+RESPOSTA = "Maior: {maior}\nMenor: {menor}\nQuantidade de divisíveis por 3: {divisiveis_3}"
 
-print(
-    RESPOSTA.format_map(
-        {
-            "maior": inteiros[-1],
-            "menor": inteiros[0],
-            "divisiveis_3": len([i for i in inteiros if i % 3 == 0]),
-        }
-    )
-)
+print(RESPOSTA.format_map(
+    {"maior": inteiros[-1],
+    "menor": inteiros[0],
+    "divisiveis_3": len([i for i in inteiros if i % 3 == 0])
+    }))
