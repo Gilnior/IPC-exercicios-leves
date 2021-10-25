92c53d560e7dd09ee7339c66827f42ee5af197d0
Gilnior <gilnior123@gmail.com>
2021-10-25 10:10:07 -0300

    problema 4 feito

---
+++ problema4.py
@@ -0,0 +1,8 @@
+"""
+Escreva um programa que solicita ao usuário um número inteiro de 4 algarismos e o programa 
+imprime na tela o seu valor invertido.
+"""
+
+num = input("Digite um inteiro de 4 algarismos: ")
+
+print("Valor invertido:", num[-1::-1])
