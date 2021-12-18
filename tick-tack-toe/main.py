8a7dfdd67fab826742cb9694f3056624f5352d6f
Gilnior <gilnior123@gmail.com>
2021-12-17 21:53:36 -0300

    Pequenos incrementos e renomeação da classe Tabuleiro para Table

--- tick-tack-toe/main.py
+++ tick-tack-toe/main.py
@@ -1,4 +1,7 @@
-class Tabuleiro:
+# Nome: Gilmar Júnio de Macêdo Guedes
+# Número de matrícula: 2021035209
+
+class Table:
     """
     Classe do tabuleiro contendo sua estrutura e lógicas de interação
     """
@@ -31,7 +34,7 @@ class Player:
     """
     Classe dos jogadores
     """
-    def __init__(self, tabuleiro: Tabuleiro):
+    def __init__(self, tabuleiro: Table):
         self.tabuleiro = tabuleiro
         self.marca = self.tabuleiro.get_marca()
 
@@ -44,7 +47,7 @@ class Game:
     Classe com o jogo de fato, contendo sua lógica e regras
     """
     def __init__(self):
-        self.tabuleiro = Tabuleiro()
+        self.tabuleiro = Table()
         self.jogador_1 = Player(self.tabuleiro)
         self.jogador_2 = Player(self.tabuleiro)
         self.turno = self.jogador_1
@@ -54,13 +57,13 @@ class Game:
         print(
             f"\
 Instruções:\n\
-Para escolher sua jogada, basta digitar, no seu turno,\
+Para escolher sua jogada, basta digitar, no seu turno, \
 uma letra que corresponda à posição que você queira jogar \
 seguindo a seguinte referência:\n{self.tabuleiro.game}\n\
-obs: perceba que o jogo foi pensado para se jogar com uma mão \
+obs: perceba que o jogo foi pensado para se jogar com uma mão, \
 em um teclado convencional\n"
             )
-        input("Entendido? [ENTER para começar]")
+        self.check_user()
 
         while self.tabuleiro.len_posicoes_ocupadas != 9 and not self.winner:
             self.tabuleiro.show()
@@ -69,8 +72,15 @@ em um teclado convencional\n"
             valido = False
             while not valido:
                 coordenada = input("Digite onde quer jogar: ").strip()
+
                 valido = coordenada not in self.tabuleiro.posicoes_ocupadas \
                     and coordenada in self.tabuleiro.mapa.keys()
+
+                if not valido: 
+                    print("\nJogada invalida, lembrando:\n", self.tabuleiro.game)
+                    self.check_user(1)
+                    self.tabuleiro.show()
+
 
             self.turno.marcar(coordenada)
             self.has_winner()
@@ -104,6 +114,12 @@ em um teclado convencional\n"
         Checa se essa linha/coluna/diagonal tem 3 marcas do jogador e retorna booleano
         """
         return len([i[1] for i in [(p, v) for (p, v) in self.tabuleiro.mapa.items() if p in line] if i[1]==self.turno.marca])==3
+    
+    @staticmethod
+    def check_user(i=0):
+        n = 0 if i==0 else 1
+        p = ("começar","continuar")[n]
+        input(f"Entendido? [ENTER para {p}]")
 
 
 if __name__=="__main__":
